bb9afea3bab8325b9abfb8478167bf98adaa01a3
Fabricio <fabriciospfullstack@gmail.com>
2022-10-10 17:11:08 -0300

    feat: :recycle: Styles and components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import { useEffect, useState } from "react"
+import './styles/styles.scss'
+
+import { api } from "./services/api"
+import { Header } from "./components/Header"
+import { Country } from "./components/Country"
+
 function App() {
-  return <h1>Hello Vite</h1>
+  const [ data, setData ] = useState([])
+
+  async function fetchAllCountries() {
+    const response = await api.get('/all')
+    setData(response.data)
+  }
+console.log(data);
+
+  useEffect(() => {
+    fetchAllCountries()
+  }, [])
+
+  return (
+    <>
+      <Header />
+      <main>
+        <section className="list-country">
+          { data &&
+            data.map(item => (
+              <Country data={item} />
+            ))
+          }
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default App
